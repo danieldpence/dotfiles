97d38a3f8a88d61f88ac316b366c2366121d1053
Daniel Pence <danieldpence@users.noreply.github.com>
2018-10-11 21:45:36 -0500

    Add VS Code User Settings

---
+++ vscode.js
@@ -0,0 +1,37 @@
+{
+  "terminal.integrated.shell.osx": "zsh",
+  "terminal.integrated.fontFamily": "Meslo LG M DZ for Powerline",
+  "terminal.integrated.fontSize": 18,
+  "terminal.integrated.confirmOnExit": true,
+  "atomKeymap.promptV3Features": true,
+  "editor.multiCursorModifier": "ctrlCmd",
+  "editor.fontSize": 18,
+  "editor.wordWrap": "wordWrapColumn",
+  "editor.wordWrapColumn": 100,
+  "editor.tabSize": 2,
+  "prettier.requireConfig": false,
+  "workbench.colorTheme": "Cobalt2",
+  "workbench.fontAliasing": "antialiased",
+  "files.insertFinalNewline": true,
+  "emmet.includeLanguages": {
+    "erb": "html"
+  },
+  "explorer.sortOrder": "type",
+  "explorer.openEditors.visible": 0,
+  "gitlens.advanced.messages": {
+    "suppressCommitHasNoPreviousCommitWarning": false,
+    "suppressCommitNotFoundWarning": false,
+    "suppressFileNotUnderSourceControlWarning": false,
+    "suppressGitVersionWarning": false,
+    "suppressLineUncommittedWarning": false,
+    "suppressNoRepositoryWarning": false,
+    "suppressResultsExplorerNotice": false,
+    "suppressShowKeyBindingsNotice": true,
+    "suppressUpdateNotice": false,
+    "suppressWelcomeNotice": true
+  },
+  "eslint.packageManager": "yarn",
+  "window.zoomLevel": 0,
+  "gitlens.keymap": "chorded",
+  "gitlens.historyExplorer.enabled": true
+}
